989b26fe4c5b80d061f2361c84def46a3577d845
AliAshraf69420 <147829330+AliAshraf69420@users.noreply.github.com>
2024-10-21 14:57:13 +0300

    what the sigma

---
+++ Pages/Survey.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab3DB.Pages
+{
+    public class SurveyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
